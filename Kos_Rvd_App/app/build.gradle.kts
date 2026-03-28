5a520861bf3ad7807ad1b8b9eeeb466af703c8ec
luqmanadi <luqmanadiw@gmail.com>
2026-03-28 14:52:16 +0700

    Update Build Gradle App

--- Kos_Rvd_App/app/build.gradle.kts
+++ Kos_Rvd_App/app/build.gradle.kts
@@ -35,7 +35,9 @@ android {
     }
     namespace = "com.kosrvd.app"
     compileSdk {
-        version = release(36)
+        version = release(36) {
+            minorApiLevel = 1
+        }
     }
 
     defaultConfig {
